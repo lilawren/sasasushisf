e0a515e4fdd4b8c855afb68a1f18fc36c04ab387
Lawrence Li <lilawren@Lawrences-MacBook-Pro.local>
2018-12-09 00:32:45 -0800

    small details

--- src/components/Reservations/Reservations.js
+++ src/components/Reservations/Reservations.js
@@ -10,28 +10,26 @@ export default class Reservations extends Component {
   render() {
     return (
       <div className="reservations">
-        <div className="menu">
-          <Parallax
-            bgImage={require('../../assets/home/rock.jpg')}
-            bgImageAlt="interior"
-            strength={200}
-          >
-            <div className="hero">
-              <div className="content">
-                <h1>
-                  RESERVATIONS
-                </h1>
-                <div className="content-row">
-                <div className="address">
-                  <div>810 Van Ness Ave.</div>
-                  <div>San Francisco, CA 94109 </div>
-                  <div>(415) 829-8291</div>
-                </div>
-                </div>
+        <Parallax
+          bgImage={require('../../assets/home/rock.jpg')}
+          bgImageAlt="interior"
+          strength={200}
+        >
+          <div className="hero">
+            <div className="content">
+              <h1>
+                RESERVATIONS
+              </h1>
+              <div className="content-row">
+              <div className="address">
+                <div>810 Van Ness Ave.</div>
+                <div>San Francisco, CA 94109 </div>
+                <div>(415) 829-8291</div>
+              </div>
               </div>
             </div>
-          </Parallax>
-        </div>
+          </div>
+        </Parallax>
       </div>
     );
   }
