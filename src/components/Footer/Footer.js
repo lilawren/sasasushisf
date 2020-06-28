2b8551eb14e6ce6d147d20a35bfc0779ff1a2540
Lawrence Li <lilawren@Lawrences-MacBook-Pro-2.local>
2020-06-28 15:51:05 -0700

    update number

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import './Footer.scss';
+import React, { Component } from "react";
+import "./Footer.scss";
 
-import yelp from '../../assets/footer/yelp.svg';
-import ig from '../../assets/footer/instagram.svg';
-import fb from '../../assets/footer/facebook.svg';
+import yelp from "../../assets/footer/yelp.svg";
+import ig from "../../assets/footer/instagram.svg";
+import fb from "../../assets/footer/facebook.svg";
 
 export default class Footer extends Component {
   render() {
@@ -15,7 +15,7 @@ export default class Footer extends Component {
             <div>22 Peace Plz</div>
             <div>Ste 530</div>
             <div>San Francisco, CA 94115</div>
-            <div>(415) 683-9674</div>
+            <div>(628) 600-6945</div>
           </div>
           <div className="hours">
             <div>
@@ -38,15 +38,30 @@ export default class Footer extends Component {
               <div>12:00pm - 2:30pm</div>
               <div>5:00pm-9:30pm</div>
               <br />
-              <div><i>Site created by <a href="http://www.lalicode.com" target="_blank">Lawrence Li</a></i></div>
+              <div>
+                <i>
+                  Site created by{" "}
+                  <a href="http://www.lalicode.com" target="_blank">
+                    Lawrence Li
+                  </a>
+                </i>
+              </div>
             </div>
           </div>
-
         </div>
         <div className="social">
-          <a href='https://www.yelp.com/biz/sasa-san-francisco' target="_blank"><img src={yelp} /></a>
-          <a href='https://www.instagram.com/sasa_sanfrancisco' target="_blank"><img src={ig} /></a>
-          <a href='https://www.facebook.com/pages/category/Japanese-Restaurant/Sasa-Japanese-Restaurant-1275103022630839/' target="_blank"><img src={fb} /></a>
+          <a href="https://www.yelp.com/biz/sasa-san-francisco" target="_blank">
+            <img src={yelp} alt="yelp" />
+          </a>
+          <a href="https://www.instagram.com/sasa_sanfrancisco" target="_blank">
+            <img src={ig} alt="instagram" />
+          </a>
+          <a
+            href="https://www.facebook.com/pages/category/Japanese-Restaurant/Sasa-Japanese-Restaurant-1275103022630839/"
+            target="_blank"
+          >
+            <img src={fb} alt="facebook" />
+          </a>
         </div>
       </div>
     );
